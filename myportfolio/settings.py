5d464207fa73d703b53595c8a4aa97a3cf598778
Chukwujiobi <chukwujiobicanon@gmail.com>
2022-07-25 09:11:24 +0100

    Quick bug fix

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@0gmc&%wf_7pjxrcy0z2hu8rzrr&%6lrtkfds-f3@v4!+6kk#-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
         "drizzle.pythonanywhere.com",
